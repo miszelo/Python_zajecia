8b8dbaf27dc6952fceb20aff6a03f8d7b2f5dd5b
michal <michal.kawczak@gmail.com>
2022-04-09 14:30:35 +0200

    poprawki

--- zadanie15_15.03.22/zadanie15.py
+++ zadanie15_15.03.22/zadanie15.py
@@ -4,11 +4,14 @@ def zadanie15(path):
         for letter in file.read():
             if letter != " ":
                 text += letter + "#"
-    
+
     return text[:-1]
+
 
 def pomocniczaDo15(path, text):
     with open(path, "w", encoding='utf-8') as file:
         file.write(text)
 
-pomocniczaDo15("C:\\Users\\micha\\Desktop\\python_zadania\\zadanie15_15.03.22\\pythonPoprawiony.txt", zadanie15("C:\\Users\\micha\\Desktop\\python_zadania\\zadanie15_15.03.22\\python.txt"))
+
+pomocniczaDo15("C:\\Users\\micha\\Desktop\\python_zadania\\zadanie15_15.03.22\\pythonPoprawiony.txt",
+               zadanie15("C:\\Users\\micha\\Desktop\\python_zadania\\zadanie15_15.03.22\\python.txt"))
